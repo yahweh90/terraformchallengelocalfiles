5147eda5cc55c2ed56c0bd1002bc8af973f06627
Yahshua Emmanuel George Ball <130876430+yahweh90@users.noreply.github.com>
2024-03-02 20:16:48 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "server_names" {
   type        = list(string)
   default     = ["server1", "server2", "server3"]
 }
+
+variable "deployment_regions" {
+  description = " configuration for deployment regions"
+  type        = set(string)
+  default     = ["us-east-1", "eu-west-1", "ap-south-1"]
+}
