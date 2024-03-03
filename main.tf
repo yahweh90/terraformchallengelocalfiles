14452a3846a97c6788ab2d71e60ff74406b5c7f0
Yahshua Emmanuel George Ball <130876430+yahweh90@users.noreply.github.com>
2024-03-02 20:16:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "local_file" "server_names_file" {
 filename = "${path.module}/server-names.txt"
   content  = join("\n", var.server_names)
 }
+
+resource "local_file" "deployment_regions_file" {
+filename = "${path.module}/deployment-regions.txt"
+  content  = join("\n", var.deployment_regions)
+}
