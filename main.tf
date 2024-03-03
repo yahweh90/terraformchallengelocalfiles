b37578be380f81370c828dcc5f2b21356f180d6f
Yahshua Emmanuel George Ball <130876430+yahweh90@users.noreply.github.com>
2024-03-02 20:27:04 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,9 @@ resource "local_file" "deployment_regions_file" {
 filename = "${path.module}/deployment-regions.txt"
   content  = join("\n", var.deployment_regions)
 }
+
+resource "local_file" "app_config_files" {
+  for_each = var.app_settings
+  filename = "${path.module}/${each.key}-config.txt"
+  content  = "${each.key}: ${each.value}"
+}
